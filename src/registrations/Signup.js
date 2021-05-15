253c20713f330c3734b7bf9c4ee5a27b9e7f6f9e
marcos <m.rodrig.felix@gmail.com>
2021-05-14 20:52:09 -0700

    add state for user info in signup component

---
+++ src/registrations/Signup.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Signup extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            password_confirmation: '',
+            errors: ''
+        }
+    }
+
+    
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Signup;
