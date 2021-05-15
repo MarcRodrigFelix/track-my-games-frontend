02c890d23a98938b09161cbd1964fd98c017c8cf
marcos <m.rodrig.felix@gmail.com>
2021-05-14 21:03:02 -0700

    add placeholders for inputs in singup form

--- src/registrations/Signup.js
+++ src/registrations/Signup.js
@@ -30,25 +30,30 @@ class Signup extends Component {
           name="username" 
           value={this.state.username}
           onChange={this.handleOnChange}
-          />
+          placeholder="Username"
+          /> <br />
           <input 
           type="text" 
           name="email" 
           value={this.state.email} 
           onChange={this.handleOnChange}
-          />
+          placeholder="Email"
+          /><br />
           <input 
           type="password" 
           name="password" 
           value={this.state.password}
           onChange={this.handleOnChange}
-          />
+          placeholder="Password"
+          /><br />
           <input 
           type="password" 
           name="password_confirmation" 
           value={this.state.password_confirmation}
           onChange={this.handleOnChange}
-          />
+          placeholder="Confirm Password"
+          /><br />
+          <input type="submit" />
         </form>
       </div>
     )
