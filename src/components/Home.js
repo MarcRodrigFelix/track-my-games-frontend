80499538d83cd58aca149e980c56756f09fba762
marcos <m.rodrig.felix@gmail.com>
2021-05-14 20:24:40 -0700

    import link from react router dom

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const Home = (props) => {
+
+    const =
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Home;
