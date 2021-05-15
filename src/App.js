75c4a9d64b0941a4fde87bccbfda63eb66dd27f1
marcos <m.rodrig.felix@gmail.com>
2021-05-14 18:47:43 -0700

    add components directory && registrations for user login, signup, home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react'
 
 
-class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoggedIn: false,
+      user: {}
+    }
+  }
+
+  
+
+
+
   render() {
     return (
       <div>
