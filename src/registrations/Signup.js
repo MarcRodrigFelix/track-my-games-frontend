95ca3e31fc79c8dd80993f3f342daecfe7d87ba1
marcos <m.rodrig.felix@gmail.com>
2021-05-14 20:56:58 -0700

    add input form for user signup

--- src/registrations/Signup.js
+++ src/registrations/Signup.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react'
 
 class Signup extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            username: '',
-            email: '',
-            password: '',
-            password_confirmation: '',
-            errors: ''
-        }
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+      errors: ''
     }
+  }
 
-    
 
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input 
+          type="text" 
+          name="username" 
+          value={this.state.username} 
+          />
+          <input 
+          type="text" 
+          name="email" 
+          value={this.state.email} 
+          />
+          <input 
+          type="password" 
+          name="password" 
+          value={this.state.password} 
+          />
+          <input 
+          type="password" 
+          name="password_confirmation" 
+          value={this.state.password_confirmation} 
+          />
+        </form>
+      </div>
+    )
+  }
 }
 
 
