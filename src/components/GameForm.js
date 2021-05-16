738ee385f347c20ec81f1339440cef81bd20f7d6
marcos <m.rodrig.felix@gmail.com>
2021-05-15 20:20:28 -0700

    create GameForm component class

---
+++ src/components/GameForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class GameForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>New Game Form</h1>
+            </div>
+        )
+    }
+}
