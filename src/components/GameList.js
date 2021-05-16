df7d3520d00370534452854313040ac0b2b3732c
marcos <m.rodrig.felix@gmail.com>
2021-05-15 20:19:25 -0700

    create GameList file and component

---
+++ src/components/GameList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class GameList extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    GameList Here.
+                </ul>
+            </div>
+        )
+    }
+}
