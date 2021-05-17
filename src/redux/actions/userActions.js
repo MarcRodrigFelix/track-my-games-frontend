cfc02af43365e07599f4c38509f6e413463ae83f
marcos <m.rodrig.felix@gmail.com>
2021-05-17 15:20:11 -0700

    add action for login form change

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,1 +1,6 @@
 export const toggleSignup = () => ({type: 'TOGGLE_SIGNUP'})
+
+export const handleLoginFormChange = (event) => ({
+    type: 'LOGIN_FORM_CHANGE',
+    payload: {name: event.target.name, value: event.target.value}
+})
