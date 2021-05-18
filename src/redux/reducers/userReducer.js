aab8f3559c60c0246f3e22f842be7a4d50ac22cc
marcos <m.rodrig.felix@gmail.com>
2021-05-17 18:59:09 -0700

    add switch case USER_LOGIN_SUCCESS for userReducer

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -14,18 +14,19 @@ const initialState = {
 const userReducer = (state = initialState, action) => {
     switch(action.type) {
         case 'TOGGLE_SIGNUP':
-            return {...state, signup: !state.signup}
+            return {...state, signup: !state.signup};
 
         case 'LOGIN_FORM_CHANGE':
             return {...state, loginForm: {
                 ...state.loginForm,
             [action.payload.name]: action.payload.value
-        }}
+        }};
 
         case 'CREATE_USER_SUCCESS':
-console.log({ ...state, ...action.payload.user })
-            return { ...state, ...action.payload.user }
+            return { ...state, ...action.payload.user };
 
+        case 'USER_LOGIN_SUCCESS':
+            return { ...state, ...action.payload.user };
         default:
             return { ...state }
     }
