63146003716120f4f9095e62f4a36f79162ce4db
marcos <m.rodrig.felix@gmail.com>
2021-05-22 21:06:00 -0700

    add Navbar.js to render navbar in each page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-// import Home from './components/Home';
 import Login from './registrations/Login';
-// import GameList from './components/GameList';
 import GameContainer from './containers/GameContainer';
 import { autoLogin, logout } from './redux/actions/userActions';
 import UserContainer from './containers/UserContainer';
-// import GameForm from './components/GameForm';
-// import UserHome from './components/UserHome';
+import GameForm from './components/GameForm';
 
 
 // render user login, signup, signout:
@@ -32,9 +29,9 @@ class App extends Component {
         <div>
           <Switch>
               {/* LOAD GAMES FROM USER ID ONLY */}
-            {/* <Route exact path='/games' component={ GameList } /> */}
+            <Route exact path='/newgames' component={ GameForm } />
             <Route path='/games' component={ GameContainer } />
-            <Route path='/' component={ UserContainer } />
+            <Route path='/user' component={ UserContainer } />
 
             {/* <Route exact path='/games/new' component={ GameForm } /> */}
           </Switch>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
 import  { BrowserRouter as Router } from 'react-router-dom';
+import store from './redux/store';
 
 
 
