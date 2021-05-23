74ef7c4cdf0741f36201de67be3c7883e084812f
marcos <m.rodrig.felix@gmail.com>
2021-05-23 13:21:33 -0700

    add game info to table

--- src/components/GameTable.js
+++ src/components/GameTable.js
@@ -25,13 +25,20 @@ export default class GameTable extends Component {
                     <thead>
                         <tr>
                             <td><strong>Title</strong></td>
-                            <td><strong>Platrform</strong></td>
+                            <td><strong>Platform</strong></td>
                             <td><strong>Genre</strong></td>
                             <td><strong>Completed</strong></td>
                         </tr>
                     </thead>
                    <tbody>
-                            { gameEntries.map( (g) => <tr><td>{g[1][1].title}</td></tr> ) }
+                            { gameEntries.map( (game) => 
+                            <tr>
+                                <td>{game[1][1].title}</td>
+                                <td>{game[1][1].platform}</td>
+                                <td>{game[1][1].kind}</td>
+                                <td>{game[1][1].is_completed ? 'Finished' : 'Not Finished'}</td>
+                            </tr>
+                            )}
                    </tbody>
                 </table>
                     {/* {(Object.keys(gameData)
