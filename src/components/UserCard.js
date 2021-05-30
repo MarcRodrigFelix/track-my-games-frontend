a5e2c0e3a7a30d72b7c329659ed49a0b313199c5
marcos <m.rodrig.felix@gmail.com>
2021-05-29 23:12:08 -0700

    add css

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 
-export default class UserCard extends Component {
+const UserCard = (props) => {
 
+    return (
+        <div className="usercard-wrapper">
+            <p>Welcome {props.user.username}, to your new game tracking app. Whats that? You have a ton of games backlogged that you want to finish playing, and there are new games that you want to start playing as well, but your so behind you don't know where to start? Well then, this is the app for you. Just add your game you want to track, then once you finish it, tell the app that and then your all done! Keep the ones you've finsihed and add more to the list to keep playing.</p>
+            < Navbar />
+        </div>
+        
+    )
 
-    render() {
-
-        return (
-            <div>
-                <h3>{this.props.user.username} page</h3>
-                <p>Welcome to your new game tracking app. Whats that? You have a ton of games backlogged that you want to finish playing, and there are new games that you want to start playing as well, but your so behind you don't know where to start? Well then, this is the app for you. Just add your game you want to track, then once you finish it, tell the app that and then your all done! Keep the ones you've finsihed and add more to the list to keep playing.</p>
-                < Navbar />
-            </div>
-            
-        )
-    }
 }
 
+export default UserCard;
